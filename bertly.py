9676d3e45b8cb3153a5e45ced067f0b9b172912b
Matt Holford <mholford@dosomething.org>
2018-03-16 17:54:39 -0400

    Initial commit: Adds bertly.py Flask app, working Serverless files

---
+++ bertly.py
@@ -0,0 +1,77 @@
+#
+# https://pythonhosted.org/shorten/user/examples.html
+#
+
+import redis
+from flask import Flask, request, redirect, url_for
+from flask import jsonify as _jsonify
+from shorten import RedisStore, NamespacedFormatter, UUIDTokenGenerator
+from shorten import alphabets
+from shorten import RevokeError
+from rfc3987 import parse
+from werkzeug import iri_to_uri
+from urlparse import urlparse
+
+compose_redis_url = os.environ['COMPOSE_REDIS_URL']
+
+ssl_wanted=compose_redis_url.startswith("rediss:")
+parsed = urlparse(compose_redis_url)
+ssl_wanted=compose_redis_url.startswith("rediss:")
+redis_client = redis.StrictRedis(
+    host=parsed.hostname,
+    port=parsed.port,
+    password=parsed.password,
+    ssl=ssl_wanted,
+    decode_responses=True)
+
+formatter = NamespacedFormatter('shorten')
+token_gen = UUIDTokenGenerator()
+
+store = RedisStore(redis_client=redis_client,
+    min_length=3,
+    counter_key='shorten:counter_key',
+    formatter=formatter,
+    token_gen=token_gen,
+    alphabet=alphabets.URLSAFE_DISSIMILAR)
+
+def jsonify(obj, status_code=200):
+    obj['status'] = 'error' if 'error' in obj else 'okay'
+    res = _jsonify(obj)
+    res.status_code = status_code
+    return res
+
+def valid_url(url):
+    return bool(parse(url, rule='URI_reference'))
+
+###########################################################
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def shorten():
+    url = request.form['url'].strip()
+
+    if not valid_url(url):
+        return jsonify({'error': str(e)}, 400)
+
+    key, token = store.insert(url)
+
+    url = url_for('bounce', key=key, _external=True)
+    revoke = url_for('revoke', token=token, _external=True)
+
+    return jsonify({'url': url, 'revoke': revoke})
+
+@app.route('/<key>', methods=['GET'])
+def bounce(key):
+    try:
+        url = store[key]
+        return redirect(iri_to_uri(url))
+    except KeyError as e:
+        return jsonify({'error': 'url not found'}, 400)
+
+@app.route('/revoke/<token>', methods=['POST'])
+def revoke(token):
+    try:
+        store.revoke(token)
+    except RevokeError as e:
+        return jsonify({'error': e}, 400)
