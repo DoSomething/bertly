e4a2f7f8ccd9d1d667cb79f3ff6c78af3ecc6fbb
stickler-ci <support@stickler-ci.com>
2018-04-26 21:09:52 +0000

    Fixing style errors.

--- bertly.py
+++ bertly.py
@@ -29,15 +29,15 @@ from werkzeug import iri_to_uri
 
 app = Flask(__name__)
 
-## COMPOSE_REDIS_URL: A complete access URL for a Redis instance
+# COMPOSE_REDIS_URL: A complete access URL for a Redis instance
 compose_redis_url = os.environ.get('COMPOSE_REDIS_URL')
 if not compose_redis_url:
     app.logger.error("No Redis URL")
 
-## Establish Redis connection as redis_client
-ssl_wanted=compose_redis_url.startswith("rediss:")
+# Establish Redis connection as redis_client
+ssl_wanted = compose_redis_url.startswith("rediss:")
 parsed = urlparse(compose_redis_url)
-ssl_wanted=compose_redis_url.startswith("rediss:")
+ssl_wanted = compose_redis_url.startswith("rediss:")
 redis_client = redis.StrictRedis(
     host=parsed.hostname,
     port=parsed.port,
@@ -48,14 +48,17 @@ redis_client = redis.StrictRedis(
 formatter = NamespacedFormatter('shorten')
 token_gen = UUIDTokenGenerator()
 
+
 def jsonify(obj, status_code=200):
     obj['status'] = 'error' if 'error' in obj else 'okay'
     res = _jsonify(obj)
     res.status_code = status_code
     return res
 
+
 def valid_url(url):
     return bool(parse(url, rule='URI_reference'))
+
 
 def require_api_key(view_function):
     """Decorator to require API key in header, passed as api_key_label"""
@@ -66,25 +69,29 @@ def require_api_key(view_function):
         api_key = os.environ.get('BERTLY_API_KEY')
 
         if not api_key_label:
-            app.logger.error("No name for API key: Should be defined in environment as BERTLY_API_KEY_NAME")
+            app.logger.error(
+                "No name for API key: Should be defined in environment as BERTLY_API_KEY_NAME")
             abort(401)
         if request.headers.get(api_key_label) and request.headers.get(api_key_label) == api_key:
             return view_function(*args, **kwargs)
         else:
             received_api_key = request.headers.get(api_key_label) or "[None]"
-            app.logger.warning("Incorrect API key header: " + api_key_label + ' = ' + received_api_key)
+            app.logger.warning("Incorrect API key header: " +
+                               api_key_label + ' = ' + received_api_key)
             abort(401)
     return decorated_function
 
 ###########################################################
 
+
 # The Redis store persists original URL, shortened key, and revocation token.
 store = RedisStore(redis_client=redis_client,
-    min_length=3,
-    counter_key='shorten:counter_key',
-    formatter=formatter,
-    token_gen=token_gen,
-    alphabet=alphabets.URLSAFE_DISSIMILAR)
+                   min_length=3,
+                   counter_key='shorten:counter_key',
+                   formatter=formatter,
+                   token_gen=token_gen,
+                   alphabet=alphabets.URLSAFE_DISSIMILAR)
+
 
 @app.route('/', methods=['POST'])
 @require_api_key
