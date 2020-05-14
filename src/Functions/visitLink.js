ebc63d1d1cc8eab135868b6b060a709055fe41c4
David Furnes <david@dfurnes.com>
2020-05-14 12:22:59 -0400

    Write click events to storage bucket.

--- src/Functions/visitLink.js
+++ src/Functions/visitLink.js
@@ -1,7 +1,32 @@
 import * as Express from 'express';
+import { v4 as uuid } from 'uuid';
 
 import Link from '../Models/Link';
+import { filesystem } from '../helpers';
 import NotFoundException from '../Exceptions/NotFoundException';
+import { info } from 'heroku-logger';
+
+/**
+ * Write this click to our storage bucket.
+ *
+ * @param {Link} link
+ * @param {Express.Request} req
+ */
+function storeClickEvent(link, req) {
+  const clickId = `${link.key}-${uuid()}`;
+
+  const payload = {
+    click_id: clickId,
+    shortened: link.key,
+    target_url: link.url,
+    click_time: new Date().toISOString(),
+    user_agent: req.headers['user-agent'] || 'N/A',
+  };
+
+  info('Logged click event.', payload);
+
+  filesystem().put(`${clickId}.json`, JSON.stringify(payload));
+}
 
 /**
  * Visit a shortlink.
@@ -17,10 +42,9 @@ export default async function visitLink(req, res) {
     throw new NotFoundException('Not found.');
   }
 
-  // Increment this link's counter:
+  // Increment this link's counter & log analytics info:
   Link.update({ key: link.key }, { $ADD: { counter: 1 } });
-
-  // TODO: Log this "click" to our storage bucket.
+  storeClickEvent(link, req);
 
   return res.redirect(link.url);
 }
