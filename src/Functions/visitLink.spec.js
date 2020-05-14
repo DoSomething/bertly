c893999763e0abd81fbb0e80847e9cc7589dfe12
David Furnes <david@dfurnes.com>
2020-05-14 10:14:15 -0400

    Add "visit link" route.

---
+++ src/Functions/visitLink.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="jest" />
+
+import { visit } from '../testing';
+import Link from '../Models/Link';
+import { dropTable } from '../helpers';
+
+beforeEach(() => dropTable(Link));
+
+describe('visitLink', () => {
+  test('It should redirect', async () => {
+    await Link.create({ key: 'xyz828f', url: 'http://vote.dosomething.org' });
+
+    const response = await visit(`/xyz828f`);
+
+    expect(response.status).toBe(302);
+    expect(response.headers.location).toBe('http://vote.dosomething.org');
+  });
+
+  test('It should handle missing links', async () => {
+    const response = await visit(`/xyz123/info`);
+
+    expect(response.status).toBe(302);
+    expect(response.headers.location).toBe('http://www.dosomething.org/404');
+  });
+});
