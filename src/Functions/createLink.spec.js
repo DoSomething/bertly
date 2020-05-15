2d6cbaf5cda8b59862b9793b95e23ff164da7959
David Furnes <david@dfurnes.com>
2020-05-15 12:58:38 -0400

    Add tests for long URLs.

--- src/Functions/createLink.spec.js
+++ src/Functions/createLink.spec.js
@@ -27,4 +27,18 @@ describe('createLink', () => {
     expect(response.body).toHaveProperty('key', link.key);
     expect(response.body).toHaveProperty('url', link.url);
   });
+
+  test('It can shorten very long URLs', async () => {
+    const url =
+      'https://www.dosomething.org/us/articles/13-ways-the-sports-world-' +
+      'is-stepping-up-during-the-coronavirus-pandemic?utm_source=content' +
+      '_campaign&utm_medium=sms&utm_campaign=sms_weekly_2020_05_11&user_' +
+      'id=55be62d4469c64182b91992b&broadcast_id=6v1RJUUmrWN2Ode0EW6lH';
+
+    const response = await postJson('/', { url });
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('key');
+    expect(response.body).toHaveProperty('url', url);
+  });
 });
--- src/Functions/visitLink.spec.js
+++ src/Functions/visitLink.spec.js
@@ -7,7 +7,7 @@ jest.mock('uuid', () => {
   return { v4: () => 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx' };
 });
 
-beforeEach(() => dropTable(Link));
+beforeEach(async () => await dropTable(Link));
 
 describe('visitLink', () => {
   test('It should redirect', async () => {
@@ -29,9 +29,24 @@ describe('visitLink', () => {
   });
 
   test('It should handle missing links', async () => {
-    const response = await visit(`/xyz123`);
+    const response = await visit(`/abc123`);
 
     expect(response.status).toBe(302);
     expect(response.headers.location).toBe('http://www.dosomething.org/404');
+  });
+
+  test('It can redirect to very long URLs', async () => {
+    const url =
+      'https://www.dosomething.org/us/articles/13-ways-the-sports-world-' +
+      'is-stepping-up-during-the-coronavirus-pandemic?utm_source=content' +
+      '_campaign&utm_medium=sms&utm_campaign=sms_weekly_2020_05_11&user_' +
+      'id=55be62d4469c64182b91992b&broadcast_id=6v1RJUUmrWN2Ode0EW6lH';
+
+    const link = await Link.create({ key: '8df6332', url });
+
+    const response = await visit(`/${link.key}`);
+
+    expect(response.status).toBe(302);
+    expect(response.headers.location).toBe(url);
   });
 });
