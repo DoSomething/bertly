7334369e816791b64fe2f3d57796720d50573e61
David Furnes <david@dfurnes.com>
2020-05-15 13:14:33 -0400

    Add link to more context in PR comment.

--- src/Functions/visitLink.js
+++ src/Functions/visitLink.js
@@ -42,9 +42,9 @@ export default async function visitLink(req, res) {
     throw new NotFoundException('Not found.');
   }
 
-  // Update the link's "visits" counter & log for analytics:
-  // TODO: We should be able to finish this asynchronously after
-  // sending the response, but it causes Jest issues. <...>
+  // Update the link's "visits" counter & log details for analytics:
+  // TODO: We should be able to finish this asynchronously after sending
+  // the response, but it causes Jest issues. <https://git.io/JfRa3>
   await Link.update({ key: link.key }, { $ADD: { counter: 1 } });
   storeClickEvent(link, req);
 
