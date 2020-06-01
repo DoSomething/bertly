fd6bb317667d875d35bb29ae2620b10b22594294
David Furnes <david@dfurnes.com>
2020-06-01 16:27:53 -0400

    Trace any outgoing HTTP requests.

--- bootstrap/lambda.js
+++ bootstrap/lambda.js
@@ -5,6 +5,9 @@ if (process.env.ENABLE_ENHANCED_XRAY) {
   var AWSXRay = require('aws-xray-sdk');
   var dynamoose = require('dynamoose');
   dynamoose.AWS = AWSXRay.captureAWS(require('aws-sdk'));
+
+  // Capture any HTTP requests made from app code:
+  AWSXRay.captureHTTPsGlobal(require('http'));
 }
 
 // We use the 'aws-serverless-express' package to run a familiar Express.js
