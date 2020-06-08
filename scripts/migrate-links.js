340b7262bf75131a14db9ac2bec6bbf893280623
David Furnes <david@dfurnes.com>
2020-06-08 14:49:08 -0400

    Write Redis/PostgreSQL to DynamoDB/S3 script. (#74)
    * Write Redis/PostgreSQL to DynamoDB/S3 script.
    
    * Split scripts & swap Redis driver.
    
    * Make sure to set 'createdAt' on migrated links.
    
    * Remove temporary override & add comment.
    
    * Fix up comment to be clearer.

---
+++ scripts/migrate-links.js
@@ -0,0 +1,45 @@
+import AWS from 'aws-sdk';
+import redis from 'redis';
+import { promisify } from 'util';
+import { info } from 'heroku-logger';
+
+import Link from '../src/Models/Link';
+
+// Configure storage connections for AWS, Redis, and RDS:
+AWS.config.update({ region: 'us-east-1' });
+
+const redisClient = redis.createClient({ url: process.env.REDIS_URL });
+const redisGet = promisify(redisClient.get).bind(redisClient);
+const redisScan = promisify(redisClient.scan).bind(redisClient);
+
+// Helper to cleanly run a Redis SCAN & track counter as we go.
+// Adapted from: https://dev.to/eastpole/redis-async-generators-3ed
+const keysMatching = async function* (pattern, initialCursor = '0') {
+  let cursor = initialCursor;
+
+  do {
+    info('Scanning for links', { cursor });
+
+    const [newCursor, keys] = await redisScan(cursor, 'MATCH', pattern);
+
+    for (const key of keys) {
+      yield key;
+    }
+
+    cursor = newCursor;
+  } while (cursor !== '0');
+};
+
+(async () => {
+  // Iterate over our existing links and migrate them to Bertly 2.0's storage:
+  for await (const redisKey of keysMatching('bertly:key:*')) {
+    const key = redisKey.replace('bertly:key:', '');
+    const url = await redisGet(redisKey);
+
+    // Write link record to DynamoDB:
+    info('Migrating shortlink.', { key, url });
+    await Link.update({ key }, { url, createdAt: new Date() });
+  }
+
+  console.log('Done!');
+})();
